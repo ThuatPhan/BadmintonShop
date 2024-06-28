90fe9f9ddccb6030391dbabc2cef013d42a14050
NguyenBao <lenguyenbaoct2003@gmail.com>
2024-06-28 21:53:18 +0700

    fix hien thi gia tien viet trang chu

--- Client/src/components/Content.jsx
+++ Client/src/components/Content.jsx
@@ -124,7 +124,7 @@ const Content = () => {
                                                                 <h4>{product.name}</h4>
                                                                 <p>{product.description}</p>
                                                                 <div className="d-flex justify-content-between flex-lg-wrap">
-                                                                    <p className="text-dark fs-5 fw-bold mb-0">{product.price.toLocaleString()}VND</p>
+                                                                    <p className="text-dark fs-5 fw-bold mb-0">{product.price.toLocaleString()}đ</p>
                                                                     <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary">
                                                                         <i className="fa fa-shopping-bag me-2 text-primary"></i>
                                                                         Add to cart
